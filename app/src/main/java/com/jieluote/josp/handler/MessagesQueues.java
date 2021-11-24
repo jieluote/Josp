a7a6227bf8b9d00e59a5ebd15e06a44654cb7353
aj <aj@imlaidian.com>
2021-11-24 18:19:54 +0800

    MessageQueue优化next()实现

--- app/src/main/java/com/jieluote/josp/handler/MessagesQueues.java
+++ app/src/main/java/com/jieluote/josp/handler/MessagesQueues.java
@@ -5,7 +5,7 @@ import android.util.Log;
 import java.util.ArrayList;
 
 public class MessagesQueues {
-    private ArrayList<Messages> queues = new ArrayList<Messages>(); //用list模拟链表 源码中messageQueue是单向链表结构
+    private final ArrayList<Messages> queues = new ArrayList<Messages>(); //用list模拟链表 源码中messageQueue是单向链表结构
     private Messages mMessages;
     private boolean mQuitting;
     private boolean mBlocked;
@@ -23,16 +23,16 @@ public class MessagesQueues {
             } else {
                 queues.add(msg);
             }
-            //如果之前的阻塞了,那么只要来了新的msg都将唤醒
+            //如果之前已阻塞,那么只要来了新的msg都将唤醒MessagesQueues(因为新来的可能要立马执行)
             if (mBlocked) {
                 //源码中通过nativeWake(mPtr)唤醒
+                Log.d("HandlersTest", "enqueueMessage notify");
                 this.notify();
             }
         }
     }
 
     public Messages next() throws InterruptedException {
-        final long now = SystemClock.uptimeMillis();
         synchronized (this) {
             if (mQuitting) {
                 return null;
@@ -40,19 +40,27 @@ public class MessagesQueues {
             if (queues.isEmpty()) {
                 return null;
             }
-            Messages msg = queues.get(0);//总是取链表头的数据,因为它的延时时间最短
-            if (msg != null) {
-                //如果还没到执行时间,就阻塞
-                if (now < msg.when) {
-                    int nextPollTimeoutMillis = (int) Math.min(msg.when - now, Integer.MAX_VALUE);
-                    Log.d("HandlersTest", "nextPollTimeoutMillis:" + nextPollTimeoutMillis / 1000L);
-                    mBlocked = true;
-                    //源码中通过native方法进行阻塞 nativePollOnce(ptr, nextPollTimeoutMillis);
-                    this.wait(nextPollTimeoutMillis);
+            int nextPollTimeoutMillis = 1;
+            for (; ;) {
+                //源码中通过native方法进行阻塞 nativePollOnce(ptr, nextPollTimeoutMillis);
+                this.wait(nextPollTimeoutMillis);
+                final long now = SystemClock.uptimeMillis();
+                Messages msg = queues.get(0);//总是取链表头的数据,因为它的延时时间最短
+                if (msg != null) {
+                    if (now < msg.when) {
+                        //计算出阻塞时间(如果还没到执行时间,就阻塞)
+                        nextPollTimeoutMillis = (int) Math.min(msg.when - now, Integer.MAX_VALUE);
+                        Log.d("HandlersTest", "nextPollTimeoutMillis:" + nextPollTimeoutMillis / 1000L);
+                        mBlocked = true;
+                    } else {
+                        mBlocked = false;
+                        queues.remove(msg);
+                        return msg;
+                    }
+                } else {
+                    nextPollTimeoutMillis = 1;
                 }
             }
-            queues.remove(msg);
-            return msg;
         }
     }
 
